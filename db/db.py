a556a8d503d0bdef12d25dd74d8f23b9b45a21b5
Sara <skis630@aucklanduni.ac.nz>
2019-08-22 00:40:09 +0300

    added 2 api's

---
+++ db/db.py
@@ -0,0 +1,51 @@
+import pymysql, json
+from bottle import response
+
+conn = pymysql.connect(host='localhost',
+                       user='root',
+                       password='Gojuryu2',
+                       db='store',
+                       charset='utf8',
+                       cursorclass=pymysql.cursors.DictCursor)
+
+def add_category(name):
+    if not name:
+        return json.dumps({"STATUS": "ERROR", "CODE": 400, "MSG": "Name parameter is missing"})
+    try:
+        with conn.cursor() as cursor:
+            sql = f"SELECT name FROM categories WHERE name = '{name}'"
+            result = cursor.execute(sql)
+            print(result)
+            if result:
+                return json.dumps({"STATUS": "ERROR", "CODE": 200, "MSG": "Category already exists"})
+            sql = f"INSERT INTO categories (name) VALUES ('{name}')"
+            cursor.execute(sql)
+            conn.commit()
+            id = cursor.lastrowid
+            response.status = 201
+            code = 201
+            status = "SUCCESS"
+            return json.dumps({"STATUS": status, "CAT_ID": id, "CODE": code})
+    except Exception as e:
+        return json.dumps({"STATUS": "ERROR", "CODE": 500, "MSG": repr(e)})
+
+
+def delete_Category(id):
+    try:
+        with conn.cursor() as cursor:
+            sql = f"SELECT id FROM categories WHERE id = '{id}'"
+            result = cursor.execute(sql)
+            print(result)
+            if not result:
+                return json.dumps({"STATUS": "ERROR", "CODE": 404, "MSG": "Category not found"})
+            sql = f"DELETE FROM categories WHERE id ='{id}'"
+            cursor.execute(sql)
+            conn.commit()
+            id = cursor.lastrowid
+            response.status = 201
+            code = 201
+            status = "SUCCESS"
+            return json.dumps({"STATUS": status, "CODE": code, "MSG": "Category deleted successfully"})
+    except Exception as e:
+        return json.dumps({"STATUS": "ERROR", "CODE": 500, "MSG": repr(e)})
+
--- store.py
+++ store.py
@@ -2,17 +2,24 @@ from bottle import route, run, template, static_file, get, post, delete, request
 from sys import argv
 import json
 import pymysql
+from db import db
 
 @get("/admin")
 def admin_portal():
 	return template("pages/admin.html")
 
+@post("/category")
+def add_category():
+    name = request.forms.get("name")
+    return db.add_category(name)
 
+@delete("/category/<id:int>")
+def delete_category(id):
+    return db.delete_Category(id)
 
 @get("/")
 def index():
     return template("index.html")
-
 
 @get('/js/<filename:re:.*\.js>')
 def javascripts(filename):
@@ -29,4 +36,4 @@ def images(filename):
     return static_file(filename, root='images')
 
 
-run(host='0.0.0.0', port=argv[1])
+run(host='localhost', port=7000, debug=True, reloader=True)
